77678463742ae0c5c7f1714d679f3d63e8646ff7
iammakena@gmail.com <root@MAKENA>
2024-03-23 13:32:56 +0300

    Fixing issues

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,8 @@ This module is the __init__ module of
 the models package.
 It initializes this package.
 """
-
 from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
 
+storage = FileStorage()
 storage.reload()
